d78aac01db3f4b07a25df707514f989cd2ed0d2e
Weebaay <jeanpaul.dijeont@gmail.com>
2024-09-05 11:41:53 +0200

    Print the number of arguments and their values passed to the script

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-num_arg = len(sys.argv) - 1
+if __name__ == "__main__":
 
-if num_arg == 0:
-    print("0 arguments.")
-elif num_arg == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(num_arg))
+    num_arg = len(sys.argv) - 1
 
-for i in range(1, len(sys.argv)):
-    print("{}: {}".format(i, sys.argv[i]))
+    if num_arg == 0:
+        print("0 arguments.")
+    elif num_arg == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_arg))
+
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
