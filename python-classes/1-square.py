dc59e1e97d4c557ad1151afdadd96d59b7c1f2f5
Weebaay <jeanpaul.dijeont@gmail.com>
2024-09-18 12:34:35 +0200

    add suare class with private size attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
 class Square:
+    """
+    A class used to represent a Square.
+
+    ...
+
+    Attributes
+    ----------
+    size : int
+        The size of one side of the square (private attribute).
+
+    Methods
+    -------
+    __init__(self, size):
+        Initializes the Square instance with a specific size.
+
+    """
+
     def __init__(self, size):
+        """
+        Parameters
+        ----------
+        size : int
+            The size of one side of the square.
+        """
         self.__size = size
-        pass 
