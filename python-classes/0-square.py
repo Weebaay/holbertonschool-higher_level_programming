358599130a70fd230f472741a75a66ac2243ce3b
Weebaay <jeanpaul.dijeont@gmail.com>
2024-09-18 12:27:45 +0200

    add empty square class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Class that defines a square.
+
+    This is an empty class that serves as the foundation for future developments,
+    where functionality such as size management, area calculation, and other square-related
+    operations will be added in subsequent exercises.
+
+    Attributes:
+        None at the moment, since this class only defines the basic structure.
+
+    Methods:
+        None yet. Future methods will provide functionality for this class.
+    """
     pass
