8e1e3de0a85022ff6f06c489ce9d0e40e7e72a12
Weebaay <jeanpaul.dijeont@gmail.com>
2024-09-19 10:23:07 +0200

    add empty for rectangle class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module 0-rectangle: define empty rectangle class"""
+
+
+class Rectangle:
+    """
+    Class that defines a rectangle.
+
+    This is an empty class that serves as the foundation for future
+    developments, where functionality such as size management,
+    area calculation, and other rectangle
+    operations will be added in subsequent exercises.
+    
+    Attributes:
+        None at the moment, since this class only defines the basic structure.
+
+    Methods:
+        None yet. Future methods will provide functionality for this class.
+    """
+    pass
