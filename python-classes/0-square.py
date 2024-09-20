8a6c2509c975e4e94743379ce339ff710cdf382e
Weebaay <jeanpaul.dijeont@gmail.com>
2024-09-20 10:35:25 +0200

    empty square class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 """Module 0-square: define an empty class Square."""
 
+
 class Square:
     """
     Class that defines a square.
-
-    This is an empty class that serves as the foundation for future
-    developments, where functionality such as size management,
-    area calculation, and other square-related
-    operations will be added in subsequent exercises.
-
-    Attributes:
-        None at the moment, since this class only defines the basic structure.
-
-    Methods:
-        None yet. Future methods will provide functionality for this class.
     """
     pass
