8aa19b23b96e2c8cde66faa6660e8f27e5c19761
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-01 14:26:14 +0200

    implement function to append text to a file and returnnumber of character added

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module 2-append_write: Define append to a file"""
+
+
+def append_write(filename="", text=""):
+    """that appends a string at the end of a text file UTF8 and return the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
