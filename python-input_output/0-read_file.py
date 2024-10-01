6a2de0b8d546a4a8df2433ce78b9a3049d87e345
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-01 14:24:46 +0200

    implement function to read and print content of a file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/biin/python3
+""" Module 0-read_file : Define read file"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content, end="")
