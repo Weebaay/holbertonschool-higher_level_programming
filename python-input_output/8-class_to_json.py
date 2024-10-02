898d989c9b58cb980edbb14e3e1883a5e621c0c6
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-02 10:42:04 +0200

    added the class to json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3*
+"""Module 8-class_to_json : Define class to json"""
+
+
+import json
+
+def class_to_json(obj):
+    """return the dictionary description with simple data structure
+    for JSON serialization of object"""
+    return obj.__dict__
+    
