456b470a8a228f6989cad451cabde69e525b7c05
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-02 10:55:31 +0200

    to json string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 3-to_json_string : Define To JSON string"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """that return the JSON reprensation of an objet(string)"""
+    return json.dumps(my_obj)
