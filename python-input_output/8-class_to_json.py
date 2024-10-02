a4389bf6cca8b1e523d5c4e1d0b45c56e4492797
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-02 14:41:24 +0200

    the dictionary description for json serialization of object

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,9 +2,7 @@
 """Module 8-class_to_json : Define class to json"""
 
 
-
 def class_to_json(obj):
     """return the dictionary description with simple data structure
     for JSON serialization of object"""
     return obj.__dict__
-    
