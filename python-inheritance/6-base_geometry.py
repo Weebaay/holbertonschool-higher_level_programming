ba5ac34d80022e37f92361b2f9f342ede3712cac
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-06 10:46:10 +0200

    add method area for classgeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry: Define class use public instane method"""
+
+
+class BaseGeometry:
+    """class basegeometry use public instance method"""
+
+    def area(self):
+        """that raise an Exception with message"""
+        raise Exception("area() is not implemented")
+    
