89d5eee9df2eeb5465ea2d210897f4638938366a
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-06 11:44:15 +0200

    class rectangle inherits from a basegeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module 8-rectangle: define class rectaangle inherits from basegeometry"""
 
+
 class BaseGeometry:
     """BaseGeometry is a base class for geometric shapes"""
 
@@ -15,13 +16,13 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class inherits from BaseGeometry"""
-    
+
     def __init__(self, width, height):
         """initialize rectangle with width and height"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-        
