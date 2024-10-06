bf1e849d041b677f0727a642ccc7d888d88dca39
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-06 11:06:57 +0200

    add basegeometry class with area and inter_validator methods

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module 7-base_geometry: define add public instance method"""
+
+
+class BaseGeometry:
+    """class basegeometry use public instance method"""
+
+    def area(self):
+        """raise an Exception with message"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
