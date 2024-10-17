1218868ea92ad41e982bd6c7449e9b18d03e60ed
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-17 15:33:04 +0200

    simple api update

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -24,8 +24,8 @@ def home():
 @app.route('/data', methods=['GET'])
 def get_users():
     """Route to get a list of all usernames"""
-    usernames = list(users.keys())
-    return jsonify(usernames)
+    user_keys = list(users.keys())
+    return jsonify(user_keys)
 
 
 @app.route('/status', methods=['GET'])
@@ -39,17 +39,17 @@ def status():
 def get_user(username):
     """Route to get details of a specific user by username."""
     user = users.get(username)
-    if user:
-        return jsonify(user)
-    else:
+    if not user:
         return jsonify({"error": "User not found"}), 404
+    else:
+        return jsonify(user)
 
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
     """Route to add a new user."""
     data = request.get_json()
-    username = data.get('username')
+    username = data.get("username")
 
     if not username:
         return jsonify({"error": "Username is required"}), 400
@@ -58,6 +58,6 @@ def add_user():
     return jsonify({"message": "User added", "user": data}), 201
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Main entry point of the application."""
-    app.run(debug=True)
+    app.run
