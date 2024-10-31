2e31271e8df1bdb058ff4aea33fcc8f828c92313
Weebaay <jeanpaul.dijeont@gmail.com>
2024-10-31 14:09:12 +0100

    city of california found in database

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- List all the cities of california
-USE hbtn_0d_usa;
 SELECT id , name
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC
+ORDER BY id ASC;
